8dcc984c2e71e00ae1c213135a7b1f3d9553c749
Shashank Rathore <Snehal Singh>
2026-03-26 22:51:34 +0530

    Deploy: Replace hardcoded values with environment variables

--- backend-ledger/server.js
+++ backend-ledger/server.js
@@ -5,6 +5,8 @@ const connectToDB = require("./src/config/db")
 
 connectToDB()
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
